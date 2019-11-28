bea9ed719533d854898dcd6ec60512782a1d4a2e
Amakhin <amakhinruslan@gmail.com>
2019-11-28 20:21:46 +0200

    Created UITableView

--- Countries/ViewController.swift
+++ Countries/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let countiesList = ["USA", "UK", "Italy", "Russia", "Spain", "Canada", "Australia", "Finland", "Ukraine"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return countiesList.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countriesCellID", for: indexPath)
+        
+        cell.textLabel?.text = countiesList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+}
